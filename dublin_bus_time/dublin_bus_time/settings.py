5d049104a0c3c79aebca05c20b0bae88bf816adf
RobShanahanUCD <rob.shanahan@ucdconnect.ie>
2020-08-04 22:15:03 +0100

    Key hiding, API restrictions

--- dublin_bus_time/dublin_bus_time/settings.py
+++ dublin_bus_time/dublin_bus_time/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!c!aw$e0b+#tbq#mid%!77qqrl1797y6ci_+t(%r@*@lskn(hg'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
