2463e8b9f16175870579f4b4faf23cd3f916fe5a
JohnParksUCD <john.parks@ucdconnect.ie>
2020-08-13 01:26:51 +0100

    Creating timetable info
    Not finished

--- dublin_bus_time/bus_stops/admin.py
+++ dublin_bus_time/bus_stops/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import BusStops, FutureWeatherData, LiveWeatherData, Weather, Journey
+from .models import BusStops, FutureWeatherData, LiveWeatherData, Weather, Journey, Timetable
 
 
 admin.site.register(BusStops)
@@ -7,6 +7,7 @@ admin.site.register(FutureWeatherData)
 admin.site.register(LiveWeatherData)
 admin.site.register(Weather)
 admin.site.register(Journey)
+admin.site.register(Timetable)
 
 
 
