cd28122a6b708935e7b49b7ce2df09bdfe095554
JohnParksUCD <john.parks@ucdconnect.ie>
2020-08-14 10:44:42 +0100

    Revert "Update Timetable"
    This reverts commit db9e753cc6feceebf172ddad69c9ea05387f7a59.

--- dublin_bus_time/bus_stops/views.py
+++ dublin_bus_time/bus_stops/views.py
@@ -158,7 +158,7 @@ class GetTimetable(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['distinct'] = Timetable.objects.values('route', 'origin').distinct('route', 'origin')
+        context['distinct'] = Timetable.objects.values('route', 'origin').distinct()
         return context
         #return {'QuerySet':QuerySet, 'distinct':distinct}
         #return render(request, "timetable.html", {'time_data': QuerySet})
